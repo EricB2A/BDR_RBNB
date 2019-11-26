b8a7ce7ed2b33d6cf9c3fb76984b3df8d2459db4
ilias <ilias.goujgali@cpnv.ch>
2019-11-26 12:04:07 +0100

    wip

--- airbnb-schema.sql
+++ airbnb-schema.sql
@@ -17,21 +17,22 @@ CREATE TABLE commune (
 CREATE TABLE adresse (
    id INT AUTO_INCREMENT,
    commune_nom VARCHAR(50),
-   rue VARCHAR(50),
+   rue VARCHAR(50) NOT NULL,
    complement_rue VARCHAR(50),
    numero VARCHAR(10),
-   npa VARCHAR(10), -- uUuU mais si on est pas en Suisse on fait comment ?
+   npa VARCHAR(10) NOT NULL, -- uUuU mais si on est pas en Suisse on fait comment ?
    PRIMARY KEY (id) ,
    FOREIGN KEY (commune_nom) REFERENCES commune(nom)
 );
 CREATE TABLE personne (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50),
-    prenom VARCHAR(50),
-    email VARCHAR(254), -- => rfc5321
-    mot_de_Passe VARCHAR(32),
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(254) NOT NULL, -- => rfc5321
+    mot_de_passe VARCHAR(64) NOT NULL,
     adresse_id INT,
     genre ENUM('Homme', 'Femme', 'Agender', 'Pangender', 'Androgyne', 'Genre fluide' ),
+    UNIQUE (email),
     FOREIGN KEY (adresse_id) REFERENCES adresse(id)
 );
 
@@ -41,9 +42,9 @@ CREATE TABLE type_bien (
 
 CREATE TABLE  bien_immobilier (
    id INT AUTO_INCREMENT PRIMARY KEY,
-   adresse_id INT,
-   type_bien_nom VARCHAR(50),
-   proprietaire_id INT,
+   adresse_id INT NOT NULL,
+   type_bien_nom VARCHAR(50) NOT NULL,
+   proprietaire_id INT NOT NULL,
    taille SMALLINT, -- en m^2
    capacite SMALLINT, -- n° de places
    description VARCHAR(10000),
@@ -56,13 +57,13 @@ CREATE TABLE  bien_immobilier (
 
 CREATE TABLE location (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    date_arrivee DATE, -- changé de date
-    duree INT,
-    estConfirme BOOLEAN,
-    locataire_id INT,
-    bien_immo_id INT,
+    date_arrivee DATE NOT NULL, -- changé de date
+    duree INT NOT NULL,
+    estConfirme BOOLEAN NOT NULL DEFAULT 0,
+    locataire_id INT NOT NULL,
+    bien_immobilier_id INT NOT NULL,
     FOREIGN KEY (locataire_id) REFERENCES personne(id),
-    FOREIGN KEY (bien_immo_id) REFERENCES bien_immobilier(id)
+    FOREIGN KEY (bien_immobilier_id) REFERENCES bien_immobilier(id)
 );
 CREATE TABLE type_fourniture(
     nom VARCHAR(50) PRIMARY KEY
@@ -71,25 +72,25 @@ CREATE TABLE type_fourniture(
 CREATE TABLE fourniture (
    id INT AUTO_INCREMENT PRIMARY KEY,
    description VARCHAR(10000),
-   bien_immo_id INT,
-   nom_fourniture VARCHAR(50),
+   bien_immobilier_id INT NOT NULL,
+   nom_fourniture VARCHAR(50) NOT NULL,
    FOREIGN KEY (nom_fourniture) REFERENCES type_fourniture(nom),
-   FOREIGN KEY (bien_immo_id) REFERENCES bien_immobilier(id)
+   FOREIGN KEY (bien_immobilier_id) REFERENCES bien_immobilier(id)
 );
 
 CREATE TABLE message (
     id INT PRIMARY KEY AUTO_INCREMENT,
     contenu VARCHAR(500),
-    location_id INT,
-    author_id INT,
+    location_id INT NOT NULL,
+    author_id INT NOT NULL,
     FOREIGN KEY (location_id) REFERENCES location(id),
     FOREIGN KEY (author_id) REFERENCES personne(id)
     
 );
 CREATE TABLE review (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    note SMALLINT, -- CI: à limité à 5 ou 10 à voir
+    note SMALLINT NOT NULL, -- CI: à limité à 5 ou 10 à voir
     commentaire VARCHAR(500),
-    location_id INT,
+    location_id INT NOT NULL,
     FOREIGN KEY (location_id) REFERENCES locations(id)
 );
--- schema_relationel.md
+++ schema_relationel.md
@@ -1,13 +1,47 @@
 # Projet BDR - Schéma relationnel
-Pays( <u>nom</u> )
-Commune(<u>nom</u>, etat, nom_pays)
-Adresse(<u>id</u>, rue, complementRue, numero, npa, nom_commune)
-Personne(<u>id</u>, nom, prenom, email, motDePasse, genre, adresse_id)
-TypeBien(<u>nom</u>)
-BienImmobilier(<u>id</u>, taille, capacite, description, tarifJournalier, charges)
-TypeFourniture(<u>nom</u>)
-Fourniture(<u>id</u>, description)
-Location(<u>id</u>, date, duree, estConfirme)
-Review(<u>id</u>, note, commentaire)
-Message(<u>id</u>, contenu)
+pays( <u>nom</u> )
 
+commune(<u>nom</u>, etat, nom_pays, pays_nom)  
+commune.pays_nom <i>référence</i> pays.nom
+
+adresse(<u>id</u>, rue, complementRue, numero, npa, nom_commune)
+rue, npa NOT NULL    
+adresse.nom_commune <i>référence</i> commune.nom
+
+personne(<u>id</u>, nom, prenom, email, motDePasse, genre, adresse_id)
+nom, prenom, email, mot_de_passe NOT NULL
+email UNIQUE  
+
+type_bien(<u>nom</u>)  
+
+bien_immobilier(<u>id</u>, taille, capacite, description, tarif_journalier, 
+charges, adresse_id, nom_type_bien_nom, proprietaire_id)  
+charges, tarif_journalier NOT NULL  
+bien_immobilier.proprietaire_id <i>référence</i> Personne.id  
+bien_immobilier.adresse_id <i>référence</i> Adresse.id  
+bien_immobilier.type_bien_nom <i>référence</i> type_bien.nom  
+
+type_fourniture(<u>nom</u>)
+
+fourniture(<u>id</u>, description, nom_type_fourniture, bien_immobilier_id)  
+bien_immobilier_id, nom_fourniture NOT NULL
+fourniture.nom_type_fourniture <i>référence</i> Type_fourniture.nom 
+fourniture.bien_immobilier_id <i>référence</i> Bien_immobilier.id
+
+location(<u>id</u>, date_arrivee, duree, estConfirme, bien_immobilier_id, locataire_id)  
+location.bien_immobilier_id <i>référence</i> bien_immobilier.id
+location.locataire_id <i>référence</i> personne.id
+
+review(<u>id</u>, note, commentaire, location_id)
+note NOT NULL
+review.location_id <i>référence</i> location.id
+
+message(<u>id</u>, contenu, location_id, author_id)  
+location_id, author_id NOT NULL
+message.author_id <i>référence</i> personne.id
+meessage.location_id <i>référence</i> location.id
+
+ 
+ 
+ 
+ 
