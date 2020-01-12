347644ab3b6f71fddc9ac2c71c691616f675f59d
Thomas Ricci <grisou13@hotmail.fr>
2020-01-12 12:27:54 +0100

    Started adding relationships

--- src/app/db/entity.py
+++ src/app/db/entity.py
@@ -3,11 +3,33 @@ class Entity:
    Meta class for all entities
    """
    _dirty = {}
+   _data = {}
+
    table_name = ""
-   def __init__(self):
+   fields = {}
+
+   relationships = {
+      "user": OneToOne("users", "id", "user_id"),
+      
+   }
+
+   def __init__(self, **fields):
       # set table name based on class
+      self.table_name = self.table_name if len(self.table_name) > 0 else self.__name__.lower()
+      # build relationship mapping
+      # fill the entity with all the fields
+      self._fill(fields)
       pass
-   
+   @classmethod
+   def build(cls, **fields):
+      new_entity = cls() 
+      new_entity._fill(fields)
+      return new_entity
+
+   def _fill(self, **fields):
+      self._dirty = filter(lambda f : f in self.fields.keys(), fields)
+      self._relationships = filter(lambda f : f in self.relationships.keys(), fields)
+
    def findOne(self, criteria):
       pass
    def find(self, criteria):
@@ -18,12 +40,18 @@ class Entity:
       pass
    def delete(self):
       pass
+
+   @abstractmethod
+   def render(self):
+      pass
+
    def _save(self, fields, action):
       pass
    def _clean(self, statement):
       pass
    def __setattr__(self, name, val):
-      if name in self.fields:
+      if name in self.fields.keys():
          self._dirty[name] = val
-      
+      elif name in self.relationships.keys():
+         self._relationships[name] = self.relationships[name].build(val)
    
---
+++ src/app/db/relationships/relationship.py
@@ -0,0 +1,11 @@
+class Relationship:
+   from_table = ""
+   local_key = ""
+   foreign_key = ""
+   foreign_table = ""
+
+   def build(self, **data):
+      pass
+
+   def save(self):
+      pass
