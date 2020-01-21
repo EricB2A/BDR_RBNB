1aa5cf06037a45484bc85ca57e0b4c73b09561e0
Thomas Ricci <grisou13@hotmail.fr>
2020-01-21 19:24:51 +0100

    wip

---
+++ src/app/gui/pages/my_rentals.py
@@ -0,0 +1,9 @@
+from app.gui.page import Page
+from app.gui.gui import Gui
+
+main = Page("my_locations", title="Mes locations")
+
+
+#En mode Locataire
+#Afficher les locations passé
+#Afficher les locations futurs
