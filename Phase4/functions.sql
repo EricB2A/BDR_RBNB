291bd6b086f615bd1d84fe6282f51a1f251d9352
AloisChristen <alois.christen@heig-vd.ch>
2020-01-21 19:41:19 +0100

    new data

--- Phase4/functions.sql
+++ Phase4/functions.sql
@@ -1,7 +1,25 @@
 -- Fonction pour inserer une nouvelle location
-DELIMITER//
+DROP PROCEDURE IF exists insert_location;
+DELIMITER //
 CREATE FUNCTION insert_location(loc_id int, bien_immo_id int, date_arrivee date, duree int)
-RETURN boolean
-
+RETURNS boolean
+DETERMINISTIC
+BEGIN
+    DECLARE done BOOLEAN;
+    IF (SELECT COUNT(id) FROM personne WHERE id = loc_id) = 1
+        AND (SELECT COUNT(id) FROM bien_immobilier WHERE id = bien_immo_id) = 1
+        AND (date_arrivee < NOW()) 
+		THEN
+        --    INSERT INTO location (locataire_id, bien_immobilier_id, date_arrivee, duree)
+          --  VALUE (loc_id, bien_immo_id, date_arrivee, duree);
+            SET done = TRUE;
+		ELSE 
+            SET done = FALSE;
+            
+        END IF;
+        RETURN done;
 END //
 
+DELIMITER ;
+
+SELECT (insert_location(1, 1, NOW(), 2));
