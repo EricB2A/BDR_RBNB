d360874d89439403ffbbc277f7bd6d5c0268bcf4
Eric Broutba <eric.bousbaa@hotmail.com>
2020-01-22 20:37:29 +0100

    décommente 🙏

--- Phase4/functions.sql
+++ Phase4/functions.sql
@@ -1,21 +1,21 @@
 use airbnb;
 
--- Fonction qui vérifie si deux périodes se chevauchent
--- DROP FUNCTION IF EXISTS dates_surperposees;
--- CREATE FUNCTION dates_surperposees(date1 DATE, date2 DATE, duree1 int, duree2 int)
--- RETURN boolean
--- DECLARE reponse boolean
---     IF (date1 BETWEEN date2 AND ADDDATE(date2, duree2)
---         OR ADDDATE(date1, duree1) BETWEEN date2 AND ADDDATE(date2, duree2)
---         OR date2 BETWEEN date1 AND ADDDATE(date1, duree1)) THEN
---         SET reponse = TRUE;
---     ELSE
---         SET reponse = FALSE;
---     END IF;
--- RETURN reponse;
--- END //
-
 DELIMITER //
+-- Fonction qui vérifie si deux périodes se chevauchent
+DROP FUNCTION IF EXISTS dates_surperposees;
+CREATE FUNCTION dates_surperposees(date1 DATE, date2 DATE, duree1 int, duree2 int)
+RETURN boolean
+DECLARE reponse boolean
+    IF (date1 BETWEEN date2 AND ADDDATE(date2, duree2)
+        OR ADDDATE(date1, duree1) BETWEEN date2 AND ADDDATE(date2, duree2)
+        OR date2 BETWEEN date1 AND ADDDATE(date1, duree1)) THEN
+        SET reponse = TRUE;
+    ELSE
+        SET reponse = FALSE;
+    END IF;
+RETURN reponse;
+END //
+
 -- Vérifie si une location a déjà lieu dans l'intervalle donnée.
 DROP FUNCTION IF EXISTS bien_est_occupe;
 CREATE FUNCTION bien_est_occupe(
